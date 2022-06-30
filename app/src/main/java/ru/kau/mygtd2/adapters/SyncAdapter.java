681239999d235c3c16ae55e8b293b07441bae3f5
Lelik2020 <ak.work76@gmail.com>
2022-06-30 22:36:43 +0300

    1

---
+++ app/src/main/java/ru/kau/mygtd2/adapters/SyncAdapter.java
@@ -0,0 +1,38 @@
+package ru.kau.mygtd2.adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SyncAdapter extends RecyclerView.Adapter<SyncAdapter.ViewHolder>{
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView title;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+
+}
