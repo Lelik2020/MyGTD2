686c7c7f3666ff52716a9ec2acf285c805737b6f
Lelik2020 <ak.work76@gmail.com>
2022-07-05 17:43:23 +0300

    добавление в файл URL для доступа на работе

--- app/src/main/java/ru/kau/mygtd2/dialogs/IPAddressEnterDialog.java
+++ app/src/main/java/ru/kau/mygtd2/dialogs/IPAddressEnterDialog.java
@@ -7,6 +7,9 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+//import android.widget.EditText;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
@@ -35,20 +38,27 @@ public class IPAddressEnterDialog extends DialogFragment {
     }
 
     com.google.android.material.textfield.TextInputEditText txtEditIPAddess;
+    EditText txtEditIPAddr;
     IPAddressEnterDialogListener callback4;// = (IPAddressEnterDialogListener) ((MainActivity) a).getSupportFragmentManager().findFragmentById(R.id.frame_container);
 
-    @SuppressLint("ResourceType")
+    @SuppressLint({"ResourceType", "MissingInflatedId"})
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        //final AlertDialog alertDialog = (AlertDialog) dialog;
+        AlertDialog.Builder builder = new AlertDialog.Builder(a);
         // Get the layout inflater
-        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        //LayoutInflater inflater = requireActivity().getLayoutInflater();
+        LayoutInflater inflater = ((MainActivity)a).getLayoutInflater();
         //txtEditIPAddess = (com.google.android.material.textfield.TextInputEditText) inflater.inflate(R.id.txtEditIPAddess2, null);
         // Inflate and set the layout for the dialog
         // Pass null as the parent view because its going in the dialog layout
-        View v = inflater.inflate(R.layout.dialog_editipaddress, null);
-        txtEditIPAddess = (com.google.android.material.textfield.TextInputEditText) v.findViewById(R.id.txtEditIPAddess2);
-        builder.setView(inflater.inflate(R.layout.dialog_editipaddress, null))
+        //View v = inflater.inflate(R.layout.dialog_editipaddress2, null);
+        //View view = (LinearLayout) getLayoutInflater().inflate(R.layout.dialog_editipaddress2, null);
+        //View view = LayoutInflater.from(a).inflate(R.layout.dialog_editipaddress2, null, false);
+        View view = inflater.inflate(R.layout.dialog_editipaddress2, null, false);
+        //txtEditIPAddess = (com.google.android.material.textfield.TextInputEditText) view.findViewById(R.id.txtEditIPAddess2);
+        //txtEditIPAddr = (EditText) view.findViewById(R.id.txtEditIPAddr);
+        builder.setView(inflater.inflate(R.layout.dialog_editipaddress2, null))
                 // Add action buttons
                 .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
@@ -57,6 +67,9 @@ public class IPAddressEnterDialog extends DialogFragment {
                         //listener.onDialogPositiveClick(((com.google.android.material.textfield.TextInputEditText)v.findViewById(R.id.txtEditIPAddess2)).getText().toString());
                         //callback4.onDialogPositiveClick(String.valueOf(txtEditIPAddess.getText()));
                         //callback4.onDialogPositiveClick(txtEditIPAddess.get);
+                        txtEditIPAddr = (EditText) ((AlertDialog)dialog).findViewById(R.id.txtEditIPAddr);
+                        //System.out.printf("222  " + txtEditIPAddr.getText().toString());
+                        callback4.onDialogPositiveClick(String.valueOf(txtEditIPAddr.getText()));
                     }
                 })
                 .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
