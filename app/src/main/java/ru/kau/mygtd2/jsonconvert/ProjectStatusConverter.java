5890ee7e345a8bbe3512102ce5bc9c887c5915f3
Lelik2020 <ak.work76@gmail.com>
2022-08-25 17:58:51 +0300

    345345

--- app/src/main/java/ru/kau/mygtd2/jsonconvert/ProjectStatusConverter.java
+++ app/src/main/java/ru/kau/mygtd2/jsonconvert/ProjectStatusConverter.java
@@ -1,6 +1,9 @@
 package ru.kau.mygtd2.jsonconvert;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
@@ -10,9 +13,15 @@ import java.lang.reflect.Type;
 import ru.kau.mygtd2.common.enums.PrStatus;
 
 
-public class ProjectStatusConverter implements JsonSerializer<PrStatus>{
+public class ProjectStatusConverter implements JsonSerializer<PrStatus>, JsonDeserializer<PrStatus> {
     @Override
     public JsonElement serialize(PrStatus src, Type typeOfSrc, JsonSerializationContext context) {
         return new JsonPrimitive(src.Value);
+    }
+
+    @Override
+    public PrStatus deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        //return null;
+        return PrStatus.from(json.getAsInt());
     }
 }
