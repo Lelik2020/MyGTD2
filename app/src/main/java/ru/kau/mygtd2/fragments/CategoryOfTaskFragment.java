a85728d6c99a69a1c5f8ca21180dc4d15620e2a0
Lelik2020 <ak.work76@gmail.com>
2023-06-22 23:18:12 +0300

    123

--- app/src/main/java/ru/kau/mygtd2/fragments/CategoryOfTaskFragment.java
+++ app/src/main/java/ru/kau/mygtd2/fragments/CategoryOfTaskFragment.java
@@ -1,11 +1,57 @@
 package ru.kau.mygtd2.fragments;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import ru.kau.mygtd2.R;
+import ru.kau.mygtd2.adapters.TaskCategoryAdapter;
+import ru.kau.mygtd2.adapters.TaskTypesAdapter;
+import ru.kau.mygtd2.common.MyApplication;
+import ru.kau.mygtd2.objects.TaskCategory;
 
 public class CategoryOfTaskFragment  extends Fragment {
 
+    TaskCategoryAdapter taskCategoryAdapter;
+
+    @SuppressLint("ResourceAsColor")
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.categoryoftask_fragment, null);
+
+        RecyclerView lvCategoryTypes = (RecyclerView) rootView.findViewById(R.id.lvCategoryOfTask);
+        final List<TaskCategory> lstTaskCategory = MyApplication.getDatabase().taskCategoryDao().getAll();
 
 
+        GradientDrawable drawable = new GradientDrawable();
+        drawable.setStroke(2, R.color.black_1);
+        drawable.setShape(GradientDrawable.RECTANGLE);
+        drawable.setCornerRadius(20);
 
+        //ShapeDrawable drawable = new ShapeDrawable();
+        //drawable.getPaint().setColor(Color.BLACK);
+        //drawable.getPaint().setStrokeWidth(5);
+
+
+        lvTaskTypes.setBackground(drawable);
+
+        lvTaskTypes.setLayoutManager(new LinearLayoutManager(getActivity()));
+        tskTypesAdapter = new TaskTypesAdapter( getActivity(), lstTaskTypes);
+
+        lvTaskTypes.setAdapter(tskTypesAdapter);
+
+        return rootView;
+
+
+    }
 
 }
